f53cd0623618e809c1e5cb541b2e50a76d66aef6
deji-ice <ayodejiatanda1@gmail.com>
2021-10-11 09:58:54 +0100

    first counter commit

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+let count  = 0;
+console.log(saveEl);
+
+function increment()
+{
+  count += 1;
+  countEl.innerText =  count;
+
+}
+function save()
+{
+  let nigga = count + "  - "
+  saveEl.textContent += nigga;
+  countEl.textContent = 0
+  count = 0
+  console.log(nigga)
+}
+
